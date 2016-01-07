ba0927f57c4c32da0aac011d73cb10f736e252b8
Shaul Hameed <shaul@poshmark.com>
2016-01-07 04:27:24 -0800

    Added Jquery terminal

--- app/assets/javascripts/libraries.js
+++ app/assets/javascripts/libraries.js
@@ -1,1 +1,4 @@
 //= require bootstrap/dist/js/bootstrap.js
+//= require jquery.terminal/js/jquery.terminal-min
+//= require jquery.terminal/js/jquery.mousewheel-min
+//= require jquery.terminal/js/unix_formatting
